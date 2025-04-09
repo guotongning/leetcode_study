922250ce877841ff39fa7d85f9accd80b0231afb
guotongning <guotongning@126.com>
2025-04-09 21:06:29 +0800

    [square] 麻瓜题，直接先平方再排序就行了。

--- src/main/java/arr/Main.java
+++ src/main/java/arr/Main.java
@@ -1,5 +1,7 @@
 package main.java.arr;
 
+import java.util.Arrays;
+
 /**
  * 数组相关算法题
  */
@@ -61,4 +63,25 @@ public class Main {
         }
         return l;
     }
+
+    /**
+     * 有序数组的平方
+     * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+     * 示例 1：
+     * 输入：nums = [-4,-1,0,3,10]
+     * 输出：[0,1,9,16,100]
+     * 解释：平方后，数组变为 [16,1,0,9,100]
+     * 排序后，数组变为 [0,1,9,16,100]
+     * 示例 2：
+     * 输入：nums = [-7,-3,2,3,11]
+     * 输出：[4,9,9,49,121]
+     */
+    public static int[] square(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
 }
