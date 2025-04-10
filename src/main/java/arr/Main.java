02b56a38b5bb3b1d4a6f3da714b29928946d8a4c
guotongning <guotongning@126.com>
2025-04-10 11:04:11 +0800

    [算法题] 新增长度最小的子数组（滑动窗口）

--- src/main/java/arr/Main.java
+++ src/main/java/arr/Main.java
@@ -14,7 +14,8 @@ public class Main {
 
     public static void main(String[] args) {
 //        System.out.println(search(ORDERLY_NUMS, 11));
-        System.out.println(removeElement(ORDERLY_NUMS, 5));
+//        System.out.println(removeElement(ORDERLY_NUMS, 5));
+        System.out.println(minSubArrayLen(ORDERLY_NUMS, 15));
     }
 
     /**
@@ -84,4 +85,27 @@ public class Main {
         return nums;
     }
 
+    /**
+     * 长度最小的子数组
+     * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+     * 示例：
+     * 输入：s = 7, nums = [2,3,1,2,4,3]
+     * 输出：2
+     * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+     * 提示：
+     * 1 <= target <= 10^9
+     * 1 <= nums.length <= 10^5
+     * 1 <= nums[i] <= 10^5
+     */
+    public static int minSubArrayLen(int[] nums, int target) {
+        int l = 0, s = 0, len = Integer.MAX_VALUE;
+        for (int r = 0; r < nums.length; r++) {
+            s += nums[r];
+            while (s >= target) {
+                len = Math.min(len, r - l + 1);
+                s -= nums[l++];
+            }
+        }
+        return len == Integer.MAX_VALUE ? 0 : len;
+    }
 }
